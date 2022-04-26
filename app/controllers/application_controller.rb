7a79d5f42ecd28810df4864d69205b2a3a519313
osha7 <ogroetz@gmail.com>
2022-04-26 04:41:09 +0000

    exchange rate tests pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,44 @@
 class ApplicationController < ActionController::Base
+
+  def home
+    render({ :template => "home.html.erb" })
+  end
+
+  def array_of_conversions 
+    exchange_rate = "https://api.exchangerate.host/symbols"
+    data = open(exchange_rate).read
+    parsed_data = JSON.parse(data)
+    return parsed_data["symbols"].keys
+  end
+
+  def forex
+    @array_of_conversions = array_of_conversions()
+
+    render({ :template => "forex.html.erb" })
+  end
+
+  def conversion_1
+    @conversion_a = params.fetch("conversion_1")
+    
+    @array_of_conversions = array_of_conversions()
+
+    render({ :template => "conversion_1.html.erb" })
+  end
+
+  def conversion_2
+    @conversion_a = params.fetch("conversion_1")
+    @conversion_b = params.fetch("conversion_2")
+
+    rate_converted = "https://api.exchangerate.host/convert?from=#{@conversion_a}&to=#{@conversion_b}"
+    data = open(rate_converted).read
+    parsed_data = JSON.parse(data)
+    @rate = parsed_data["info"]["rate"]
+
+    render({ :template => "conversion_2.html.erb" })
+  end
+
+  def covid
+    render({ :template => "covid.html.erb" })
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get("/", { :controller => "application", :action => "home" })
+  get("/forex", { :controller => "application", :action => "forex" })
+  get("/forex/:conversion_1", { :controller => "application", :action => "conversion_1" })
+  get("/forex/:conversion_1/:conversion_2", { :controller => "application", :action => "conversion_2" })
+
+  get("/covid", { :controller => "application", :action => "covid" })
 end
